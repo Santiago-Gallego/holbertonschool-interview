ad0b67e5424983ac1dadddf53f9774a73549bcff
Santiago-Gallego <santiagoxd68@hotmail.com>
2021-03-17 22:00:45 +0000

    [Add]

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,40 +1,46 @@
-#!/usr/bin/python3
-"""Module N Queens
-   b: The b state.
-"""
 import sys
 
 
-def cposition(b, row, col):
-    """Checks position"""
-    for c in range(col):
-        if b[c] is row or abs(b[c] - row) is abs(c - col):
-            return False
-    return True
+def decode(list):
+    """
+    0. N queens
+    fn decode for square NxN
+    """
+    try:
+        if len(list[0]) != 2:
+            print("Usage: nqueens N")
+            exit(1)
+        NxN = int(list[0][1])
+        if NxN < 4:
+            print("N must be at least 4")
+            exit(1)
 
+        col = list[1]
+        listThreats = list[2]
+        for row in range(NxN):
+            nonAttacking = 1
+            for move in listThreats:
+                if move[1] == row:
+                    nonAttacking = 0
+                    break
+                if col - row == move[0] - move[1]:
+                    nonAttacking = 0
+                    break
+                if row - move[1] == move[0] - col:
+                    nonAttacking = 0
+                    break
+            if nonAttacking == 1:
+                listThreats.append([col, row])
+                if NxN - 1 != col:
+                    decode([list[0], col + 1, listThreats])
+                else:
+                    print(listThreats)
+                del listThreats[-1]
 
-def check(b, col):
-    """backtracking """
-    n = len(b)
-    if col is n:
-        print(str([[c, b[c]] for c in range(n)]))
-        return
-
-    for row in range(n):
-        if cposition(b, row, col):
-            b[col] = row
-            check(b, col + 1)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    if sys.argv[1].isdigit() is False:
+    except ValueError:
         print("N must be a number")
-        sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    b = [0 for col in range(int(sys.argv[1]))]
-    check(b, 0)
+
+# run init function for default
+decode([sys.argv, 0, []])
