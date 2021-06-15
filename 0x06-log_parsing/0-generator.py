5b6e0e8e48bc47e4230a7cf384bfb0354944ddef
Santiago Gallego <60366040+Santiago-Gallego@users.noreply.github.com>
2021-06-15 15:08:33 -0500

    Update 0-generator.py

--- 0x06-log_parsing/0-generator.py
+++ 0x06-log_parsing/0-generator.py
@@ -4,6 +4,15 @@ import sys
 from time import sleep
 import datetime
 
-for i in range(3):
-    sys.stdout.write('78.99.227.220 - [2017-02-05 23:25:51.534767] "GET /projects/260 HTTP/1.1" 401 724\n')
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write(
+        "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+            random.randint(1, 255),
+            random.randint(1, 255),
+            random.randint(1, 255),
+            random.randint(1, 255),
+            datetime.datetime.now(),
+            random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+            random.randint(1, 1024)))
     sys.stdout.flush()
