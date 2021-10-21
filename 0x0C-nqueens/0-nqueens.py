43dad17a2cfa074fd3d1e1a274f1f34bfdb6afa7
Santiago-Gallego <santiagoxd68@hotmail.com>
2021-10-21 17:47:19 -0500

    [Add]

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,79 +1,71 @@
 #!/usr/bin/python3
+
 """
-N queens puzzle is the challenge of placing N non-attacking queens
+N queens problem
 """
+
 import sys
 
-
-def printB(brd):
-    """
-    print board
-    """
-    r = []
-    for x in brd:
-        for c in x:
-            if c == 1:
-                r.append([brd.index(x), x.index(c)])
-    print(r)
-
-
-def is_safe(brd, row, col, n):
-    """
-    is_safe
-    """
-    for i in range(col):
-
-        if brd[row][i] + brd[row][i + 1] != 0:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
-
-    return True
-
-
-def nqueen(brd, col, n):
-    """
-    nqueen
-    """
-
-    if (col >= n):
-        printB(brd)
-
-    for x in range(n):
-        if is_safe(brd, x, col, n):
-            brd[x][col] = 1
-            if nqueen(brd, col+1, n):
-                return True
-            brd[x][col] = 0
-
-    return False
-
-
-# initial run starting from the col 0!
-def main():
-    """
-    Main
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if sys.argv[1].isnumeric():
-        n = int(sys.argv[1])
-    else:
-        print("N must be a number")
-        exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-    brd = [[0 for x in range(n)] for y in range(n)]
-    nqueen(brd, 0, n)
-
-
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        n_q = int(sys.argv[1])
+    except BaseException:
+        print('N must be a number')
+        sys.exit(1)
+
+    if n_q < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    board = [[0 for col in range(n_q)] for row in range(n_q)]
+
+    def main():
+        b = [[0 for j in range(n_q)] for i in range(n_q)]
+        recursive_func(b, 0)
+        return
+
+    def recursive_func(b, c):
+        if (c == n_q):
+            solution(b)
+            return True
+        ret = False
+        for i in range(n_q):
+            if (validate_pos(b, i, c)):
+                b[i][c] = 1
+                ret = recursive_func(b, c + 1) or ret
+                b[i][c] = 0
+        return ret
+
+    def validate_pos(b, r, c):
+        for i in range(c):
+            if (b[r][i]):
+                return False
+        i = r
+        j = c
+        while i >= 0 and j >= 0:
+            if(b[i][j]):
+                return False
+            i = i - 1
+            j = j - 1
+        i = r
+        j = c
+        while j >= 0 and i < n_q:
+            if(b[i][j]):
+                return False
+            i = i + 1
+            j = j - 1
+        return True
+
+    def solution(b):
+        solve = []
+        for i in range(n_q):
+            for j in range(n_q):
+                if(b[i][j] == 1):
+                    solve.append([i, j])
+        print(solve)
+        solve.clear()
     main()
